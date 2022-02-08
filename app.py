a9b105d8f68d4cce56c09ecf445716f0be6404fe
Bertrand Kotewall <bkotewall@gmail.com>
2022-02-07 22:22:19 -0500

    added the flask routes.  just need to get them to work now

--- app.py
+++ app.py
@@ -1,27 +1,51 @@
 import datetime
 import os
 
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, jsonify
 
-from models import Signups
+from models import Book 
 from database import db_session
 
 app = Flask(__name__)
 app.secret_key = os.environ['APP_SECRET_KEY']
 
-@app.route("/", methods=('GET', 'POST'))
-def signup():
-    form = SignupForm()
-    if form.validate_on_submit():
-        signup = Signups(name=form.name.data, email=form.email.data, date_signed_up=datetime.datetime.now())
-        db_session.add(signup)
-        db_session.commit()
-        return redirect(url_for('success'))
-    return render_template('signup.html', form=form)
+@app.route("/api/v1.0/help", methods=('GET'))
+def help():
+    return(
+    '''
+    Welcome to the Oreilly Book API!
+    Available Routes:
+    /
+    /api/v1.0/help
+    /api/v1.0/title/<book title>
+    /api/v1.0/bookid/start/end
+    /api/v1.0/add_book/
+    ''')
 
-@app.route("/success")
-def success():
-    return "Thank you for signing up!"
+
+@app.route("/api/v1.0/title", methods=('GET'))
+def title_search():
+    return 0
+
+@app.route("/api/v1.0/bookid/start")
+@app.route("/api/v1.0/bookid/start/end") 
+def bookid_lookup(start=None, end=None):
+    if not end:
+        results = Book.query(id=start)
+    else:
+        results = session.query.\
+        filter(Book.id >= start).\
+        filter(Book.id <= end).all()
+    return jsonify(results)
+
+@app.route("/api/v1.0/add_book", methods=('POST'))
+def add_book():
+    return 0
+
+@app.route("/", methods=('GET'))
+def index():
+    results = Book.query.all()
+    return jsonify(reults)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5090, debug=True)

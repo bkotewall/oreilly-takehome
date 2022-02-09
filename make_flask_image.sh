78bd47a49af2ed4cda8eaca03c846e41d1f604fb
Bertrand Kotewall <bkotewall@gmail.com>
2022-02-09 00:37:15 -0500

    some scripting shortcuts

---
+++ make_flask_image.sh
@@ -0,0 +1,4 @@
+docker login
+docker build --no-cache -t oreilly-takehome_flaskapp  .
+docker tag oreilly-takehome_flaskapp:latest  bkotewall/oreilly-takehome_flaskapp:latest
+docker push bkotewall/oreilly-takehome_flaskapp:latest

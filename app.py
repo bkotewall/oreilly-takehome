f0819681b283d82dea2fa19c6a97fe4ca6b41ba7
Bertrand Kotewall <bkotewall@gmail.com>
2022-02-09 00:35:15 -0500

    fixed the basic query.  the searches on titles and id ranges are still screwed.  have not tested adding data yet

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 
-from flask import Flask, render_template, redirect, url_for, jsonify
+from flask import Flask, request, jsonify
 
 from models import Book 
 from database import db_session
@@ -9,43 +9,79 @@ from database import db_session
 app = Flask(__name__)
 app.secret_key = os.environ['APP_SECRET_KEY']
 
-@app.route("/api/v1.0/help", methods=('GET'))
+@app.route("/api/help")
 def help():
-    return(
+    help_string = '''
+    Welcome to the Oreilly Book API!<br/>
+    Available Routes:<br/>
+    /<br/>
+    /api/help<br/>
+    /api/title/<book title><br/>
+    /api/bookid/start/end<br/>
+    /api/add_book/<br/>
     '''
-    Welcome to the Oreilly Book API!
-    Available Routes:
-    /
-    /api/v1.0/help
-    /api/v1.0/title/<book title>
-    /api/v1.0/bookid/start/end
-    /api/v1.0/add_book/
-    ''')
+    return(help_string)
 
 
-@app.route("/api/v1.0/title", methods=('GET'))
-def title_search():
-    return 0
+@app.route("/api/title/<title>")
+def title_search(title):
+    print(title)
+    find_title_query = Book.query.filter(Book.title.match(f"%{title}%"))
+    results = db_session.query(find_title_query)
+    return jsonify(render_results(results))
 
-@app.route("/api/v1.0/bookid/start")
-@app.route("/api/v1.0/bookid/start/end") 
+@app.route("/api/bookid/<start>")
+@app.route("/api/bookid/<start>/<end>") 
 def bookid_lookup(start=None, end=None):
-    if not end:
-        results = Book.query(id=start)
+    if not start:
+        results = {}
+    elif not end:
+        results = db_session.query(Book.id == start)
     else:
-        results = session.query.\
-        filter(Book.id >= start).\
-        filter(Book.id <= end).all()
-    return jsonify(results)
+        results = db_session.query.(Book.id >= start).\
+                  filter(Book.id <= end).all()
+    return jsonify(render_results(results))
 
-@app.route("/api/v1.0/add_book", methods=('POST'))
-def add_book():
-    return 0
+@app.route("/api/add_book/<bookid>", methods=['POST'])
+def add_book(id):
+    content = request.json
+    newbook = Book(id = bookid,
+                   isbn = content["isbn"],
+                   authors = content["authors"],
+                   title = content["title"],
+                   description = content["description"]
+                  )
+    db_session.add(newbook)
+    db_session.commit()
 
-@app.route("/", methods=('GET'))
+@app.route("/")
 def index():
-    results = Book.query.all()
-    return jsonify(reults)
+    results = db_session.query(Book.id, Book.authors, Book.title, Book.isbn,
+            Book.description).all()
+
+    all_results = []
+    for id, authors, title, isbn, description in results:
+        book_dict = {}
+        book_dict['id'] = id
+        book_dict['authors'] = authors
+        book_dict['title'] = title
+        book_dict['isbn'] = isbn
+        book_dict['description'] = description
+        all_results.append(book_dict)
+    return jsonify(all_results)
+
+def render_results(sql_alchemy_results):
+    print(sql_alchemy_results)
+    for id, authors, title, isbn, description in sql_alchemy_results:
+        book_dict = {}
+        book_dict['id'] = id
+        book_dict['authors'] = authors
+        book_dict['title'] = title
+        book_dict['isbn'] = isbn
+        book_dict['description'] = description
+        all_results.append(book_dict)
+    print(all_results)
+    return all_results
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5090, debug=True)

83f2ca6d92a38ff4fc507d51bc54c4f88a843666
Bertrand Kotewall <bkotewall@gmail.com>
2022-02-09 11:18:41 -0500

    this should fix the select by ids?

--- app.py
+++ app.py
@@ -30,17 +30,27 @@ def title_search(title):
     results = db_session.query(find_title_query)
     return jsonify(render_results(results))
 
-@app.route("/api/bookid/<start>")
+@app.route("/api/bookid/<start>/")
 @app.route("/api/bookid/<start>/<end>") 
 def bookid_lookup(start=None, end=None):
     if not start:
         results = {}
+        return jsonify(results)
     elif not end:
-        results = db_session.query(Book.id == start)
+        results = db_session.query(Book).get(start)
+        book_dict = {}
+        book_dict['id'] = results.id
+        book_dict['authors'] = results.authors
+        book_dict['title'] = results.title
+        book_dict['isbn'] = results.isbn
+        book_dict['description'] = results.description
+        return jsonify(book_dict)
+
     else:
-        results = db_session.query.(Book.id >= start).\
-                  filter(Book.id <= end).all()
-    return jsonify(render_results(results))
+        results = db_session.query(Book).filter(Book.id >= start, Book.id <=
+                end).all()
+        print(results)
+        return jsonify(render_results(results))
 
 @app.route("/api/add_book/<bookid>", methods=['POST'])
 def add_book(id):
--- make_flask_image.sh
+++ make_flask_image.sh
@@ -2,3 +2,4 @@ docker login
 docker build --no-cache -t oreilly-takehome_flaskapp  .
 docker tag oreilly-takehome_flaskapp:latest  bkotewall/oreilly-takehome_flaskapp:latest
 docker push bkotewall/oreilly-takehome_flaskapp:latest
+docker-compose up

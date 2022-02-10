7b6f82bdf9fe44ddc68a629760adf99775680db5
Bertrand Kotewall <bkotewall@gmail.com>
2022-02-10 05:32:43 -0600

    split this out to save time

---
+++ release_flask_image.sh
@@ -0,0 +1,5 @@
+docker login
+docker build --no-cache -t oreilly-takehome_flaskapp  .
+docker tag oreilly-takehome_flaskapp:latest  bkotewall/oreilly-takehome_flaskapp:latest
+docker push bkotewall/oreilly-takehome_flaskapp:latest
+docker-compose up

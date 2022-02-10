b2ec4da6917bfd6ecef497eb233412b265b3c94a
Bertrand Kotewall <bkotewall@gmail.com>
2022-02-10 12:54:38 -0600

    pep8 fixes

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ def help():
 def title_search(title):
     print(title)
     book_ans = db_session.query(Book.id, Book.authors, Book.title,
-                                        Book.isbn, Book.description).\
-                                        filter(Book.title.match(f"%{title}%")).all()
+                                Book.isbn, Book.description).\
+                                filter(Book.title.match(f"%{title}%")).all()
     return render_results(book_ans)
 
 
@@ -54,21 +54,44 @@ def bookid_lookup(start=None, end=None):
 @app.route("/api/add_book", methods=['POST'])
 def add_book():
     request_data = request.json
+    error = dict()
+    error["reason"] = ''
+
+    # let's play some defense here:
+
     if not request_data:
-        error = dict()
-        error["reason"] = "please pass the right argument"
-        results = list(np.ravel(error))
-        return jsonify(results=error)
+        return error_message_json(error, "needs data")
+
+    content = json.loads(request_data)
+
+    if not content.keys() >= {"bookid", "authors", "isbn",
+                              "description", "title"}:
+        return error_message_json(error, "please make sure all keys are present
+                                  when making api call")
+
+    elif not isinstance(content["bookid"], int):
+        return error_message_json(error, "bookid is an int")
+    elif not isinstance(content["isbn"], str):
+        return error_message_json(error, "isbn is a string of 17 chars in
+                                  length")
+    elif not isinstance(content["authors"], list):
+        return error_message_json(error, "authors is a list of strings.")
+    elif not isinstance(content["description"], str):
+        return error_message_json(error, "description is a long string.")
+    # check following:
+    # bookid is there and it's an int
+    # isbn is there and it's a string
+    # authors is an array of strings
+    # title is a string
+    # description is a string
+
     else:
-        print(request_data)
-        content = json.loads(request_data)
-        print(content)
         newbook = Book()
-        newbook.id=content["bookid"]
-        newbook.isbn=content["isbn"]
-        newbook.authors=content["authors"]
-        newbook.title=content["title"]
-        newbook.description=content["description"]
+        newbook.id = content["bookid"]
+        newbook.isbn = content["isbn"]
+        newbook.authors = content["authors"]
+        newbook.title = content["title"]
+        newbook.description = content["description"]
         db_session.add(newbook)
         db_session.commit()
         success = dict()
@@ -87,6 +110,13 @@ def index():
 def render_results(query_res):
     answer = list(np.ravel(query_res))
     return jsonify(results=answer)
+
+
+def error_message_json(error_dict, error_message):
+    error_dict["reason"] = error_message
+    prepped_data = list(np.ravel(error_dict))
+    return jsonify(results=prepped_data)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5090, debug=True)
